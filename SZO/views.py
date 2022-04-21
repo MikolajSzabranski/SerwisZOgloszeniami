adb1a4b338440037bae4a82f525b0b3fd1d21a6c
MikolajSzabranski <mikolaj.szabranski@student.wat.edu.pl>
2022-04-21 12:23:40 +0200

    1.0

--- SZO/views.py
+++ SZO/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 def home(response):
     return render(response, "SZO/home.html", {})
+
+
+def login(response):
+    return render(response, "SZO/login.html", {})
--- SerwisZOgloszeniami/settings.py
+++ SerwisZOgloszeniami/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'SZO.apps.SzoConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [

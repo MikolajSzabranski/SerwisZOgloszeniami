26ed5fca31e1394e73d8e993ea7fb5004312fd03
MikolajSzabranski <mikolaj.szabranski@student.wat.edu.pl>
2022-04-23 16:54:46 +0200

    1.3

--- SZO/forms.py
+++ SZO/forms.py
@@ -1,15 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
-from .models import CustomUser
+from .models import CustomUser, typesUser
 
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
 
+    typeUser = forms.ChoiceField(choices=typesUser, widget=forms.RadioSelect, initial='Job seeker')
+
     class Meta:
         model = CustomUser
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "email", "password1", "password2", "typeUser"]
 
 
 class CustomUserChangeForm(UserChangeForm):
--- SZO/models.py
+++ SZO/models.py
@@ -1,9 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
-
+from django import forms
 
 # Create your models here.
 
+typesUser = (('Job seeker', 'Job seeker'), ('Host', 'Host'))
 
-class CustomUser:
-    pass
+
+class CustomUser(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.EmailField(blank=True)
+    password1 = models.CharField(max_length=30)
+    password2 = models.CharField(max_length=30)
+    typeUser = models.CharField(max_length=100, choices=typesUser, null=True)

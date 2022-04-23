1464ffaf20f1e20aa358fc9df894a1b8f3ed8f98
MikolajSzabranski <mikolaj.szabranski@student.wat.edu.pl>
2022-04-23 09:16:31 +0200

    1.0

--- SZO/views.py
+++ SZO/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -10,3 +12,15 @@ def home(response):
 
 def login(response):
     return render(response, "SZO/login.html", {})
+
+
+def register(response):
+    if response.method == "POST":
+        form = UserCreationForm(response.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/login")
+    else:
+        form = UserCreationForm()
+
+    return render(response, "SZO/register.html", {"form": form})
